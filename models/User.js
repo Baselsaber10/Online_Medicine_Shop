4664e7c77f7ca829c13f29a718c58e755104f491
Tasneem Mohamed <tasneem1.mohamed1@gmail.com>
2025-11-11 12:11:52 +0200

    Add User class

---
+++ models/User.js
@@ -0,0 +1,18 @@
+class User {
+  constructor(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  register() {
+    //add logic to register user
+  }
+
+  login() {
+    //add logic to log user in
+  }
+}
+
+module.exports = User;
