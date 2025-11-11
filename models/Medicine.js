433f4b41e1ea7e8a95268892fa7d50a9faefcea8
Tasneem Mohamed <tasneem1.mohamed1@gmail.com>
2025-11-11 12:13:42 +0200

    Add & delete Medicine class

---
+++ models/Medicine.js
@@ -0,0 +1,23 @@
+class Medicine {
+  constructor(id, name, category, price, stock) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  addMedicine() {
+    //add logic to add medicine
+  }
+
+  updateStock() {
+    // update medicine stock
+  }
+
+  deleteMedicine() {
+    //delete a medicine
+  }
+}
+
+module.exports = Medicine;
