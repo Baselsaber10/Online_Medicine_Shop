168c59732fa0a04a955becb59ed5b263c0bc82ab
Tasneem Mohamed <tasneem1.mohamed1@gmail.com>
2025-11-18 05:04:23 +0200

    Enhance User model with address, phone, and methods
    Added address and phone fields to User model and implemented static methods for user creation and retrieval.

--- models/User.js
+++ models/User.js
@@ -1,17 +1,48 @@
+const db = require("../database/db");
+
 class User {
-  constructor(id, name, email, password) {
+  constructor(id, name, email, password, address, phone) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
+    this.address = address;
+    this.phone = phone;
   }
 
-  register() {
-    //add logic to register user
+  // Create user
+  static createUser(data) {
+    return new Promise((resolve, reject) => {
+      const query = `INSERT INTO users (name, email, password, address, phone)
+      VALUES (?, ?, ?, ?, ?)`;
+
+      db.run(query, [data.name, data.email, data.password, data.address, data.phone],
+        function (err) {
+          if (err) reject(err);
+          resolve({ id: this.lastID, ...data });
+        }
+      );
+    });
   }
 
-  login() {
-    //add logic to log user in
+  // Get user by email
+  static findUserByEmail(email) {
+    return new Promise((resolve, reject) => {
+      db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      });
+    });
+  }
+
+  // Get user by id
+  static findUserById(id) {
+    return new Promise((resolve, reject) => {
+      db.get("SELECT * FROM users WHERE id = ?", [id], (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      });
+    });
   }
 }
 
